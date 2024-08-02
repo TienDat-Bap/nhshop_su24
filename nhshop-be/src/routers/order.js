58691785aef2be8f6c2997aca35c8439d86970ca
TienDat-Bap <tiendat150120@gmail.com>
2024-08-02 11:15:14 +0700

    first commit

--- nhshop-be/src/routers/order.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createOrder, getOrderById, getOrders } from "../controllers/order";
-
-const router = Router();
-
-router.post("/orders", createOrder);
-router.get("/orders", getOrders);
-router.get("/orders/:userId/:orderId", getOrderById);
-export default router;
